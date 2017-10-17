040fe79d43ab0614157070d456e27b786b41792a
Kienen Wayrynen <kienen.wayrynen@wsu.edu>
2017-10-17 15:35:29 -0700

    Created registration screen

--- app/src/main/java/com/example/kienenwayrynen/myapplication/LoginScreen.java
+++ app/src/main/java/com/example/kienenwayrynen/myapplication/LoginScreen.java
@@ -3,7 +3,9 @@ package com.example.kienenwayrynen.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
 import android.view.View;
+import android.widget.Button;
 
 public class LoginScreen extends AppCompatActivity implements View.OnClickListener {
 
@@ -13,6 +15,9 @@ public class LoginScreen extends AppCompatActivity implements View.OnClickListen
 
         setContentView(R.layout.login);
 
+        Button registerButton = (Button) findViewById(R.id.register_button);
+
+        registerButton.setOnClickListener(this);
     }
 
     @Override
--- app/src/main/java/com/example/kienenwayrynen/myapplication/RegistrationScreen.java
+++ app/src/main/java/com/example/kienenwayrynen/myapplication/RegistrationScreen.java
@@ -1,7 +1,12 @@
 package com.example.kienenwayrynen.myapplication;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by kienenwayrynen on 10/5/17.
@@ -12,6 +17,22 @@ public class RegistrationScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.next_activity);
+        setContentView(R.layout.registration);
+        Button registerButton = (Button) findViewById(R.id.submit_registration_button);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextInputEditText password = (TextInputEditText)findViewById(R.id.password);
+                TextInputEditText confirmPassword = (TextInputEditText)findViewById(R.id.confirm_password);
+                if(password.getText().toString().equals(confirmPassword.getText().toString())){
+                    Log.d("click","blahloggedin");
+                }else{
+                    Toast.makeText(RegistrationScreen.this, "passwords do not match", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
     }
+
 }
